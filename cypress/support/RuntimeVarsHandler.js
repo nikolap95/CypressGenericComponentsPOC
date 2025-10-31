407b13292f0b664498460ec9f3d1a329fff6de12
nikolap95 <nikolap@oiprobotics.com>
2025-10-31 15:54:10 +0100

    Formatting fixes.

--- cypress/support/RuntimeVarsHandler.js
+++ cypress/support/RuntimeVarsHandler.js
@@ -1,12 +1,11 @@
 export class RuntimeVarsHandler {
     static #map = new Map();
-  
+
     static add(key, value) { this.#map.set(String(key), String(value)); }
-    static get(key) {
-        return this.#map.get(String(key)); }
-    static has(key)        { return this.#map.has(String(key)); }
-    static delete(key)     { this.#map.delete(String(key)); }
-    static clear()         { this.#map.clear(); }
-    static entries()       { return Array.from(this.#map.entries()); }
-    static toJSON()        { return Object.fromEntries(this.#map); }
-  }
+    static get(key) { return this.#map.get(String(key)); }
+    static has(key) { return this.#map.has(String(key)); }
+    static delete(key) { this.#map.delete(String(key)); }
+    static clear() { this.#map.clear(); }
+    static entries() { return Array.from(this.#map.entries()); }
+    static toJSON() { return Object.fromEntries(this.#map); }
+}
